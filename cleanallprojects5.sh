fb28689e4118c41caad227fc912af4d202b3b944
Narendra Umate <narendraumate@gmail.com>
2015-11-09 01:01:10 -0500

    premake.

---
+++ cleanallprojects5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+case "$OSTYPE" in
+	darwin*) ./Tools/MacOS/premake5/premake5 --verbose --file=premake5.lua --os=macosx clean;rm -rf bin obj;;
+	linux*) ./Tools/Linux/premake5/premake5 --verbose --file=premake5.lua --os=linux clean;rm -rf bin obj;;
+esac
