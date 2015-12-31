62134bc8447ba042c3f13ae0672877564e84e122
Narendra Umate <narendraumate@gmail.com>
2015-12-31 18:22:18 +0530

    premake.

--- cleanallprojects5.sh
+++ cleanallprojects5.sh
@@ -2,5 +2,5 @@
 
 case "$OSTYPE" in
 	darwin*) ./Tools/MacOS/premake5/premake5 --verbose --file=premake5.lua --os=macosx clean;rm -rf bin obj;rm -rf Framework.xc*/;;
-	linux*) ./Tools/Linux/premake5/premake5 --verbose --file=premake5.lua --os=linux clean;rm -rf bin obj;;
+	linux*) ./Tools/Linux/premake5/premake5 --verbose --file=premake5.lua --os=linux clean;rm -rf bin obj;rm Makefile Framework.make;;
 esac
