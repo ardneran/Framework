28c45502e6f1f8aa121e006810ee632f5df10414
Narendra Umate <narendraumate@gmail.com>
2016-02-22 02:02:12 -0500

    stb.

---
+++ External/stb/Include/stb_image_helper.h
@@ -0,0 +1,30 @@
+#ifndef STBIMAGE
+#define STBIMAGE
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "stb_image.h"
+
+class Image
+{
+public:
+	Image(const string& name);
+	virtual ~Image();
+	
+	inline unsigned char* getPixels() { return m_pixels; } const
+	inline int getSizeX() const { return m_sizeX; }
+	inline int getSizeY() const { return m_sizeY; }	
+
+	void flipY();
+	
+private:
+	unsigned char* m_pixels;
+	int m_sizeX;
+	int m_sizeY;
+	int m_channels;
+	int m_count;
+};
+
+#endif // STBIMAGE
