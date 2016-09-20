2b44a8806f3e28c5f0bdf74802b69296931275f8
Narendra Umate <narendraumate@gmail.com>
2016-09-20 00:53:43 -0400

    ObjMeshLoader.

--- Source/ObjMeshLoader.cpp
+++ Source/ObjMeshLoader.cpp
@@ -44,7 +44,12 @@ std::list<VisualSpatial*> ObjMeshLoader::load(const std::string& filepath, const
 			}
             VisualSpatial* visual = new VisualSpatial(positions, normals, texcoords, indices);
 			// Assume that every shape has only one material.
-			loadMaterial(visual, materials[it->mesh.material_ids[0]], filebase);
+			if (materials.size()) {
+				loadMaterial(visual, materials[it->mesh.material_ids[0]], filebase);
+			} else {
+				tinyobj::material_t material;
+				loadMaterial(visual, material, filebase);
+			}
             visual->setModelBoundingBox(positions);
             visuals.push_back(visual);
         }
@@ -56,37 +61,39 @@ std::list<VisualSpatial*> ObjMeshLoader::load(const std::string& filepath, const
 
 void ObjMeshLoader::loadMaterial(VisualSpatial* const visual, const tinyobj::material_t& material, const std::string& filebase) {
 	Material* currentMaterial = visual->getMaterial();
+	if (currentMaterial)
+	{
+		// TODO Optimize the array copies.
+		currentMaterial->ambient[0] = material.ambient[0];
+		currentMaterial->ambient[1] = material.ambient[1];
+		currentMaterial->ambient[2] = material.ambient[2];
+		currentMaterial->diffuse[0] = material.diffuse[0];
+		currentMaterial->diffuse[1] = material.diffuse[1];
+		currentMaterial->diffuse[2] = material.diffuse[2];
+		currentMaterial->specular[0] = material.specular[0];
+		currentMaterial->specular[1] = material.specular[1];
+		currentMaterial->specular[2] = material.specular[2];
+		currentMaterial->transmittance[0] = material.transmittance[0];
+		currentMaterial->transmittance[1] = material.transmittance[1];
+		currentMaterial->transmittance[2] = material.transmittance[2];
+		currentMaterial->emission[0] = material.emission[0];
+		currentMaterial->emission[1] = material.emission[1];
+		currentMaterial->emission[2] = material.emission[2];
 
-	// TODO Optimize the array copies.
-	currentMaterial->ambient[0] = material.ambient[0];
-	currentMaterial->ambient[1] = material.ambient[1];
-	currentMaterial->ambient[2] = material.ambient[2];
-	currentMaterial->diffuse[0] = material.diffuse[0];
-	currentMaterial->diffuse[1] = material.diffuse[1];
-	currentMaterial->diffuse[2] = material.diffuse[2];
-	currentMaterial->specular[0] = material.specular[0];
-	currentMaterial->specular[1] = material.specular[1];
-	currentMaterial->specular[2] = material.specular[2];
-	currentMaterial->transmittance[0] = material.transmittance[0];
-	currentMaterial->transmittance[1] = material.transmittance[1];
-	currentMaterial->transmittance[2] = material.transmittance[2];
-	currentMaterial->emission[0] = material.emission[0];
-	currentMaterial->emission[1] = material.emission[1];
-	currentMaterial->emission[2] = material.emission[2];
+		currentMaterial->shininess = material.shininess;
+		currentMaterial->ior = material.ior;
+		currentMaterial->dissolve = material.dissolve;
+		currentMaterial->illum = material.illum;
+		currentMaterial->dummy = material.dummy;
 
-	currentMaterial->shininess = material.shininess;
-	currentMaterial->ior = material.ior;
-	currentMaterial->dissolve = material.dissolve;
-	currentMaterial->illum = material.illum;
-	currentMaterial->dummy = material.dummy;
-
-	loadTexture(filebase, material.ambient_texname, currentMaterial->ambient_texname);
-	loadTexture(filebase, material.diffuse_texname, currentMaterial->diffuse_texname);
-	loadTexture(filebase, material.specular_texname, currentMaterial->specular_texname);
-	loadTexture(filebase, material.specular_highlight_texname, currentMaterial->specular_highlight_texname);
-	loadTexture(filebase, material.bump_texname, currentMaterial->bump_texname);
-	loadTexture(filebase, material.displacement_texname, currentMaterial->displacement_texname);
-	loadTexture(filebase, material.alpha_texname, currentMaterial->alpha_texname);
+		loadTexture(filebase, material.ambient_texname, currentMaterial->ambient_texname);
+		loadTexture(filebase, material.diffuse_texname, currentMaterial->diffuse_texname);
+		loadTexture(filebase, material.specular_texname, currentMaterial->specular_texname);
+		loadTexture(filebase, material.specular_highlight_texname, currentMaterial->specular_highlight_texname);
+		loadTexture(filebase, material.bump_texname, currentMaterial->bump_texname);
+		loadTexture(filebase, material.displacement_texname, currentMaterial->displacement_texname);
+		loadTexture(filebase, material.alpha_texname, currentMaterial->alpha_texname);
+	}
 }
 
 void ObjMeshLoader::loadTexture(const std::string& base, const std::string& name, Texture2D*& texture) {
